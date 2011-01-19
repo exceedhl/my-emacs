c1701b6c40346026a97a106dd402d4e151790f25
Huang Liang <exceedhl@gmail.com>
2011-01-19 15:09:17 +0800

    using rsync to sync .emacs files

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 rsync -vmrc $1 --filter=". rsync-filter" ~/.emacs.d/ ~/Desktop/my-emacs/site-lisp/ 
-cp -v ~/.emacs ~/Desktop/my-emacs/
+rsync -vc $1 ~/.emacs ~/Desktop/my-emacs/.emacs
--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 rsync -vmrc $1 --filter=". rsync-filter" ~/Desktop/my-emacs/site-lisp/ ~/.emacs.d/
-cp -v ~/Desktop/my-emacs/.emacs ~
+rsync -vc $1 ~/Desktop/my-emacs/.emacs ~/.emacs
